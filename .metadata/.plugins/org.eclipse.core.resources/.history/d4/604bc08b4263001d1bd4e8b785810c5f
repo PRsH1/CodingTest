4bbae85642f8c2c5b6002beae148246fe80935ee
PRsH1 <impjs17@gmail.com>
2022-11-14 00:08:00 +0900

    stack

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d4/604bc08b4263001d1bd4e8b785810c5f
@@ -0,0 +1,18 @@
+
+public class LinkedList {
+	private Node head;
+	private class Node{
+		Node link;
+		int temp;
+		Node(int temp){
+			this.temp=temp;
+		}
+	}
+	public void addFirstNode(int number) {
+		Node d=new Node(number);
+		d.link=head;
+		head=d;
+	}
+
+
+}
--- Stack/src/LinkedList.java
+++ Stack/src/LinkedList.java
@@ -13,6 +13,41 @@ public class LinkedList {
 		d.link=head;
 		head=d;
 	}
+	public int top() {
+		if(head==null) {
+			return -1;
+		}
+		else
+			return head.temp;
+	}
+	public int size() {
+		Node count=head;
+		int number = 0;
+		if(count==null)
+			return 0;
+		while(count!=null) {
+			count=count.link;
+			number+=1;
+		}
+		return number;
+	}
+	public int empty() {
+		if(head==null) {
+			return 1;
+		}
+		else 
+			return 0;
+	}
+	public int pop() {
+		int temp=0;
+		if(head==null)
+			return -1;
+		
+		temp=head.temp;
+		Node pop=head;
+		head=pop.link;
+		return temp;
+	}
 
 
 }
